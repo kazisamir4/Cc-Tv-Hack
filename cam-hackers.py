37b2ffcec79c25b9c3ff062f12074f49375848f9
kazi samir <97796921+kazisamir4@users.noreply.github.com>
2022-08-11 08:59:43 +0600

    Update and rename samir.py to cam-hackers.py

--- cam-hackers.py
+++ cam-hackers.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
-#github.com/AngelSecurityTeam/Cam-Hackers
+#github.com/kazisamir4/cam-hackers.py
 import requests, re , colorama
 colorama.init()
 print("""
